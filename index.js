67030d0754c90f94c01a041bcca008998c8fbc26
m-madelyn <m.madelyn@live.com>
2019-11-04 22:49:20 -0500

    post handler to add a new team

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
 const express = require('express')
 const app = express()
 const teams = require('./teams.json')
+const bodyParser = require('body-parser')
 
 app.get('/teams', (req, res) => {
     res.send(teams)
@@ -13,6 +13,21 @@ app.get('/teams/:filter', (req, res) => {
     })
 
     res.send(specificTeam)
+})
+
+app.post('/teams', bodyParser.json(), (req, res) => {
+    const body = req.body
+
+    if (!body.id || !body.location || !body.mascot || !body.abbreviation || !body.conference || !body.division) {
+        res.status(400).send('The following attributes are required: id, location, mascot, abbreviation, conference, division')
+    } else {
+
+        teams.push(body)
+        console.log({
+            body
+        })
+        res.status(201).send(body)
+    }
 })
 
 app.all('*', (req, res) => {
