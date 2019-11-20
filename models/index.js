5655265c00e455a7ee268e27095a08ddd2fef7ae
m-madelyn <m.madelyn@live.com>
2019-11-20 06:13:24 -0500

    Includes new Migrations, Models and using db instead of json file

--- models/index.js
+++ models/index.js
@@ -1,26 +1,28 @@
+//done
 
-//need to install sequelize
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize') //importing sequelize node modules
+const allConfigs = require('../config/sequelize') //importing env variables needed to set up DB connection
+const TeamsModel = require('./teams') //importing function that defines a table for 'teams'
 
-const allConfigs = require('../config/sequelize')
-const TeamsModel = require('./teams')//equivelent to writing all SQL code
 
-//drilling down to the development/db configuration (username, pw, db)
+//drilling down to the development_db config (username, pw, db)
 const config = allConfigs['development']
-//creates connection to database using the sequelize.js file
 
+//creates connection to database using the sequelize.js file
 const connection = new Sequelize(config.database, config.username, config.password, {
- //why is this separate from the rest?
-  host: config.host,
-  dialect: config.dialect,
-})  
+
+    host: config.host,
+    dialect: config.dialect
+})
 connection.authenticate()
 
-// exports an instance of the team model
-//do we need Sequelize in both places? (ie, teams.js and Teams variable)
-const Teams = TeamsModel (connection, Sequelize)
+//exports an instance of the TeamsModel
+
+const Teams = TeamsModel(connection, Sequelize)
+
 
 module.exports = {
+    //export for WEB APP Teams
     Teams,
+    
 }
-
